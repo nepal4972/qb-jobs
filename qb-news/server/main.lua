5ef805c3db99975bc47e50cf499b99ebdb30444e
Saugat Nepal <83812124+nepal4972@users.noreply.github.com>
2022-07-16 01:26:07 +0545

    Add files via upload

---
+++ qb-news/server/main.lua
@@ -0,0 +1,16 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+QBCore.Commands.Add("newscam", "Grab a news camera", {}, false, function(source, args)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if Player.PlayerData.job.name == "reporter" then
+        TriggerClientEvent("Cam:ToggleCam", source)
+    end
+end)
+
+QBCore.Commands.Add("newsmic", "Grab a news mic", {}, false, function(source, args)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if Player.PlayerData.job.name == "reporter" then
+        TriggerClientEvent("Mic:ToggleMic", source)
+    end
+end)
+
